1845b9cc3c4d33829791bf03e29b21638df22423
Shuduo Sang <sangshuduo@gmail.com>
2021-11-30 13:55:03 +0800

    [TD-11619]<fix>: make install bug on CentOS

--- packaging/tools/make_install.sh
+++ packaging/tools/make_install.sh
@@ -15,8 +15,16 @@ ${csudo} cp ${source_dir}/build/bin/taosdump /usr/local/taos/bin || echo -e "fai
 ${csudo} cp ${source_dir}/build/bin/taosdemo /usr/local/taos/bin || echo -e "failed to copy taosdemo"
 ${csudo} ln -sf /usr/local/taos/bin/taosdump /usr/local/bin/taosdump || echo -e "failed to link taosdump"
 ${csudo} ln -sf /usr/local/taos/bin/taosdemo /usr/local/bin/taosdemo || echo -e "failed to link taosdemo"
-${csudo} cp -rf ${source_dir}/build/lib/libavro* /usr/local/lib || echo -e "failed to copy avro libraries"
-${csudo} cp -rf ${source_dir}/build/lib/pkgconfig /usr/local/lib || echo -e "failed to copy pkgconfig directory"
+
+if [ -f ${source_dir}/build/lib/libavro.so.23.0.0 ];
+    ${csudo} cp -rf ${source_dir}/build/lib/libavro* /usr/local/lib > /dev/null || echo -e "failed to copy avro libraries"
+    ${csudo} cp -rf ${source_dir}/build/lib/pkgconfig /usr/local/lib > /dev/null || echo -e "failed to copy pkgconfig directory"
+fi
+
+if [ -f ${source_dir}/build/lib64/libavro.so.23.0.0 ];
+    ${csudo} cp -rf ${source_dir}/build/lib64/libavro* /usr/local/lib > /dev/null || echo -e "failed to copy avro libraries"
+    ${csudo} cp -rf ${source_dir}/build/lib64/pkgconfig /usr/local/lib > /dev/null || echo -e "failed to copy pkgconfig directory"
+fi
 
 if [ -d /etc/ld.so.conf.d ]; then
     echo "/usr/local/lib" | ${csudo} tee /etc/ld.so.conf.d/libavro.conf > /dev/null || echo -e "failed to write /etc/ld.so.conf.d/libavro.conf"
