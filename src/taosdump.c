0c24230e35fc86955ee3de53aa6216f1a332fb84
Shuduo Sang <sangshuduo@gmail.com>
2022-01-14 16:47:45 +0800

    remove unused func

--- src/taosdump.c
+++ src/taosdump.c
@@ -513,33 +513,6 @@ void prompt() {
     }
 }
 
-char* strToLower(char *dst, const char *src) {
-  int esc = 0;
-  char quote = 0, *p = dst, c;
-
-  assert(dst != NULL);
-
-  for (c = *src++; c; c = *src++) {
-    if (esc) {
-      esc = 0;
-    } else if (quote) {
-      if (c == '\\') {
-        esc = 1;
-      } else if (c == quote) {
-        quote = 0;
-      }
-    } else if (c >= 'A' && c <= 'Z') {
-      c -= 'A' - 'a';
-    } else if (c == '\'' || c == '"') {
-      quote = c;
-    }
-    *p++ = c;
-  }
-
-  *p = 0;
-  return dst;
-}
-
 int setConsoleEcho(bool on)
 {
 #define ECHOFLAGS (ECHO | ECHOE | ECHOK | ECHONL)
