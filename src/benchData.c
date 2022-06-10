3f77ad481765cbf519d0eb362cbf9680e38929dc
zhaoyanggh <yzhao@taosdata.com>
2022-06-10 16:08:27 +0800

    fix: sml line protocol binary escape

--- src/benchData.c
+++ src/benchData.c
@@ -23,6 +23,10 @@ const char* locations[] = {"San Francisco", "Los Angles", "San Diego",
                            "San Jose", "Palo Alto", "Campbell", "Mountain View",
                            "Sunnyvale", "Santa Clara", "Cupertino"};
 
+const char* locations_sml[] = {"San\\ Francisco", "Los\\ Angles", "San\\ Diego",
+                           "San\\ Jose", "Palo\\ Alto", "Campbell", "Mountain\\ View",
+                           "Sunnyvale", "Santa\\ Clara", "Cupertino"};
+
 const char* locations_chinese[] = {"旧金山","洛杉矶","圣地亚哥","圣何塞","帕洛阿尔托","坎贝尔",
                                    "山景城","森尼韦尔","圣克拉拉","库比蒂诺"};
 
@@ -749,6 +753,8 @@ void generateRandData(SSuperTable *stbInfo, char *sampleDataBuf,
                     if (g_arguments->demo_mode) {
                         if (g_arguments->chinese) {
                             sprintf(tmp, "%s", locations_chinese[taosRandom() % 10]);
+                        } else if (stbInfo->iface == SML_IFACE) {
+                            sprintf(tmp, "%s", locations_sml[taosRandom() % 10]);
                         } else {
                             sprintf(tmp, "%s", locations[taosRandom() % 10]);
                         }
