4a81884aae9607f1a76361dd86d38420195e64bb
zhaoyanggh <yzhao@taosdata.com>
2022-08-25 17:18:47 +0800

    fix: create table without if not exits

--- src/benchInsert.c
+++ src/benchInsert.c
@@ -147,8 +147,8 @@ skip:
     int length = snprintf(
         command, BUFFER_SIZE,
         stbInfo->escape_character
-            ? "CREATE TABLE IF NOT EXISTS %s.`%s` (ts TIMESTAMP%s) TAGS %s"
-            : "CREATE TABLE IF NOT EXISTS %s.%s (ts TIMESTAMP%s) TAGS %s",
+            ? "CREATE TABLE %s.`%s` (ts TIMESTAMP%s) TAGS %s"
+            : "CREATE TABLE %s.%s (ts TIMESTAMP%s) TAGS %s",
         database->dbName, stbInfo->stbName, cols, tags);
     tmfree(cols);
     tmfree(tags);
@@ -295,15 +295,15 @@ static void *createTable(void *sarg) {
             if (stbInfo->childTblCount == 1) {
                 snprintf(pThreadInfo->buffer, TSDB_MAX_SQL_LEN,
                          stbInfo->escape_character
-                         ? "CREATE TABLE IF NOT EXISTS %s.`%s` %s;"
-                         : "CREATE TABLE IF NOT EXISTS %s.%s %s;",
+                         ? "CREATE TABLE %s.`%s` %s;"
+                         : "CREATE TABLE %s.%s %s;",
                          database->dbName, stbInfo->stbName,
                          stbInfo->colsOfCreateChildTable);
             } else {
                 snprintf(pThreadInfo->buffer, TSDB_MAX_SQL_LEN,
                          stbInfo->escape_character
-                         ? "CREATE TABLE IF NOT EXISTS %s.`%s%" PRIu64 "` %s;"
-                         : "CREATE TABLE IF NOT EXISTS %s.%s%" PRIu64 " %s;",
+                         ? "CREATE TABLE %s.`%s%" PRIu64 "` %s;"
+                         : "CREATE TABLE %s.%s%" PRIu64 " %s;",
                          database->dbName, stbInfo->childTblPrefix, i,
                          stbInfo->colsOfCreateChildTable);
             }
@@ -318,9 +318,9 @@ static void *createTable(void *sarg) {
 
             len += snprintf(
                 pThreadInfo->buffer + len, TSDB_MAX_SQL_LEN - len,
-                stbInfo->escape_character ? "if not exists %s.`%s%" PRIu64
+                stbInfo->escape_character ? "%s.`%s%" PRIu64
                                             "` using %s.`%s` tags (%s) "
-                                          : "if not exists %s.%s%" PRIu64
+                                          : "%s.%s%" PRIu64
                                             " using %s.%s tags (%s) ",
                 database->dbName, stbInfo->childTblPrefix, i, database->dbName,
                 stbInfo->stbName, stbInfo->tagDataBuf + i * stbInfo->lenOfTags);
