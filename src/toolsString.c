c425a6f269b3b56d96efb3db4afeb4d8f7aa7839
Shuduo Sang <sangshuduo@gmail.com>
2022-10-29 08:40:33 +0000

    fix: co zhiqiang's 3.0 windows workflow

--- src/toolsString.c
+++ src/toolsString.c
@@ -14,48 +14,51 @@
 
 #ifdef WINDOWS
 char *strsep(char **stringp, const char *delim) {
-  char       *s;
-  const char *spanp;
-  int32_t     c, sc;
-  char       *tok;
-  if ((s = *stringp) == NULL) return (NULL);
-  for (tok = s;;) {
-    c = *s++;
-    spanp = delim;
-    do {
-      if ((sc = *spanp++) == c) {
-        if (c == 0)
-          s = NULL;
-        else
-          s[-1] = 0;
-        *stringp = s;
-        return (tok);
-      }
-    } while (sc != 0);
-  }
-  /* NOTREACHED */
+    char       *s;
+    const char *spanp;
+    int32_t     c, sc;
+    char       *tok;
+    if ((s = *stringp) == NULL) return (NULL);
+    for (tok = s;;) {
+        c = *s++;
+        spanp = delim;
+        do {
+            if ((sc = *spanp++) == c) {
+                if (c == 0)
+                    s = NULL;
+                else
+                    s[-1] = 0;
+                *stringp = s;
+                return (tok);
+            }
+        } while (sc != 0);
+    }
+    /* NOTREACHED */
 }
+
 /* Copy no more than N characters of SRC to DEST, returning the address of
    the terminating '\0' in DEST, if any, or else DEST + N.  */
 char *stpncpy(char *dest, const char *src, int n) {
-  size_t size = strnlen(src, n);
-  memcpy(dest, src, size);
-  dest += size;
-  if (size == n) return dest;
-  return memset(dest, '\0', n - size);
+    size_t size = strnlen(src, n);
+    memcpy(dest, src, size);
+    dest += size;
+    if (size == n) return dest;
+    return memset(dest, '\0', n - size);
 }
+
 char *stpcpy(char *dest, const char *src) {
-  strcpy(dest, src);
-  return dest + strlen(src);
+    strcpy(dest, src);
+    return dest + strlen(src);
 }
+
 void toolsLibFuncInclude() {
-  assert(0);
-  fread(0,0,0,0);
-  srand(0);
-  rand();
-  realloc(0, 0);
-  strtod(0, 0);
-  fputs(0, 0);
+    assert(0);
+    fread(0,0,0,0);
+    srand(0);
+    rand();
+    realloc(0, 0);
+    strtod(0, 0);
+    fputs(0, 0);
 }
 #endif
 
