ffe090d056ec4c78de89ed4de50d772a68b7599a
Shuduo Sang <sangshuduo@gmail.com>
2023-02-03 16:39:40 +0800

    fix: memory leak after use dbname

--- src/benchInsert.c
+++ src/benchInsert.c
@@ -336,6 +336,7 @@ int32_t getVgroupsOfDb(SBenchConn *conn, SDataBase *database) {
         printErrCmdCodeStr(cmd, code, res);
         return -1;
     }
+    taos_free_result(res);
 
     sprintf(cmd, "SHOW vgroups");
     res = taos_query(conn->taos, cmd);
