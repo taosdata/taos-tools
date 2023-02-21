60db5039cc4135b7551f5770f036932e0dbb5d8f
Shuduo Sang <sangshuduo@gmail.com>
2023-02-21 15:29:31 +0800

    fix: make deps/toolscJson/src/toolscJson.c same as upstream

--- deps/toolscJson/src/toolscJson.c
+++ deps/toolscJson/src/toolscJson.c
@@ -289,8 +289,6 @@ loop_end:
 
     input_buffer->offset += (size_t)(after_end - number_c_string);
 
-    strncpy(item->numberstring, (const char *)number_c_string, strlen(item->numberstring) - strlen((const char*)number_c_string) - 1);
-
     return true;
 }
 
