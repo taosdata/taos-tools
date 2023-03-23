d6c9313b218c16ef42e5c0be03eda22cf9ee7489
Shuduo Sang <sangshuduo@gmail.com>
2023-03-23 18:36:12 +0800

    fix: 2.x tag name order issue

--- tests/taosbenchmark/sml_json_insert_alltypes-same-min-max.py
+++ tests/taosbenchmark/sml_json_insert_alltypes-same-min-max.py
@@ -58,6 +58,10 @@ class TDTestCase:
             return paths[0]
 
     def run(self):
+        tdSql.query("select client_version()")
+        client_ver = "".join(tdSql.queryResult[0])
+        major_ver = client_ver.split(".")[0]
+
         binPath = self.getPath()
         cmd = (
             "%s -f ./taosbenchmark/json/sml_json_insert_alltypes-same-min-max.json"
@@ -70,14 +74,25 @@ class TDTestCase:
         rows = tdSql.queryResult[0]
         tdSql.query("select * from db.stb")
         for row in range(rows[0]):
-            tdSql.checkData(row, 1, 1.0)
-            tdSql.checkData(row, 2, 1.0)
-            tdSql.checkData(row, 4, 1.0)
-            tdSql.checkData(row, 5, 1.0)
-            tdSql.checkData(row, 6, 1.0)
-            tdSql.checkData(row, 7, 1.0)
-            tdSql.checkData(row, 11, 30.0)
-            tdSql.checkData(row, 12, 60000.0)
+            if major_ver == "3":
+                tdSql.checkData(row, 1, 1.0)
+                tdSql.checkData(row, 2, 1.0)
+                tdSql.checkData(row, 4, 1.0)
+                tdSql.checkData(row, 5, 1.0)
+                tdSql.checkData(row, 6, 1.0)
+                tdSql.checkData(row, 7, 1.0)
+                tdSql.checkData(row, 11, 30.0)
+                tdSql.checkData(row, 12, 60000.0)
+            else:
+                tdSql.checkData(row, 1, 1.0)
+                tdSql.checkData(row, 2, 1.0)
+                tdSql.checkData(row, 3, 60000.0)
+                tdSql.checkData(row, 5, 1.0)
+                tdSql.checkData(row, 6, 1.0)
+                tdSql.checkData(row, 7, 1.0)
+                tdSql.checkData(row, 8, 1.0)
+                tdSql.checkData(row, 12, 30.0)
+
 
     def stop(self):
         tdSql.close()
--- tests/taosbenchmark/sml_telnet_insert_alltypes-same-min-max.py
+++ tests/taosbenchmark/sml_telnet_insert_alltypes-same-min-max.py
@@ -58,6 +58,10 @@ class TDTestCase:
             return paths[0]
 
     def run(self):
+        tdSql.query("select client_version()")
+        client_ver = "".join(tdSql.queryResult[0])
+        major_ver = client_ver.split(".")[0]
+
         binPath = self.getPath()
         cmd = (
             "%s -f ./taosbenchmark/json/sml_telnet_insert_alltypes-same-min-max.json"
@@ -66,21 +70,36 @@ class TDTestCase:
         tdLog.info("%s" % cmd)
         os.system("%s" % cmd)
         tdSql.execute("reset query cache")
-        tdSql.query("select count(*) from db.t0")
+        tdSql.query("select count(*) from db.stb")
         rows = tdSql.queryResult[0]
         tdSql.query("select * from db.stb")
         for row in range(rows[0]):
-            tdSql.checkData(row, 3, "1i32")
-            tdSql.checkData(row, 4, "3000000000i64")
-            tdSql.checkData(row, 5, "1.000000f32")
-            tdSql.checkData(row, 6, "1.000000f64")
-            tdSql.checkData(row, 7, "1i16")
-            tdSql.checkData(row, 8, "1i8")
-            tdSql.checkData(row, 9, "true")
-            tdSql.checkData(row, 10, "4000000000u32")
-            tdSql.checkData(row, 11, "5000000000u64")
-            tdSql.checkData(row, 12, "30u8")
-            tdSql.checkData(row, 13, "60000u16")
+            if major_ver == "3":
+                tdSql.checkData(row, 1, 1)
+                tdSql.checkData(row, 2, "1i32")
+                tdSql.checkData(row, 3, "3000000000i64")
+                tdSql.checkData(row, 4, "1.000000f32")
+                tdSql.checkData(row, 5, "1.000000f64")
+                tdSql.checkData(row, 6, "1i16")
+                tdSql.checkData(row, 7, "1i8")
+                tdSql.checkData(row, 8, "true")
+                tdSql.checkData(row, 9, "4000000000u32")
+                tdSql.checkData(row, 10, "5000000000u64")
+                tdSql.checkData(row, 11, "30u8")
+                tdSql.checkData(row, 12, "60000u16")
+            else:
+                tdSql.checkData(row, 1, 1)
+                tdSql.checkData(row, 2, "1i32")
+                tdSql.checkData(row, 3, "60000u16")
+                tdSql.checkData(row, 4, "3000000000i64")
+                tdSql.checkData(row, 5, "1.000000f32")
+                tdSql.checkData(row, 6, "1.000000f64")
+                tdSql.checkData(row, 7, "1i16")
+                tdSql.checkData(row, 8, "1i8")
+                tdSql.checkData(row, 9, "true")
+                tdSql.checkData(row, 10, "4000000000u32")
+                tdSql.checkData(row, 11, "5000000000u64")
+                tdSql.checkData(row, 12, "30u8")
 
     def stop(self):
         tdSql.close()
