98d65ce9013698b5363a4751bb0dc68748e62dff
Alex Duan <417921451@qq.com>
2023-10-22 14:39:30 +0800

    fix: build error

--- src/benchData.c
+++ src/benchData.c
@@ -1601,14 +1601,14 @@ int prepareSampleData(SDataBase* database, SSuperTable* stbInfo) {
         // check valid
         if(stbInfo->partialColFrom >= stbInfo->cols->size) {
             stbInfo->partialColFrom = 0;
-            infoPrint("stbInfo->partialColFrom(%d) is large than stbInfo->cols->size(%d) \n ",stbInfo->partialColFrom,stbInfo->cols->size);
+            infoPrint("stbInfo->partialColFrom(%d) is large than stbInfo->cols->size(%"PRIu64") \n ",stbInfo->partialColFrom,stbInfo->cols->size);
         }
 
         if (stbInfo->partialColFrom + stbInfo->partialColNum > stbInfo->cols->size) {
             stbInfo->partialColNum = stbInfo->cols->size - stbInfo->partialColFrom ;
         }
 
-        if(stbInfo->partialColNum < stbInfo->cols->size)
+        if(stbInfo->partialColNum < stbInfo->cols->size) {
             stbInfo->partialColNameBuf =
                     benchCalloc(1, TSDB_MAX_ALLOWED_SQL_LEN, true);
             int pos = 0;
@@ -1622,7 +1622,7 @@ int prepareSampleData(SDataBase* database, SSuperTable* stbInfo) {
             } else {
                 pos += n;
             }
-            for (int i = stbInfo->partialColFrom; i < stbInfo->partialColNum; ++i) {
+            for (int i = stbInfo->partialColFrom; i < stbInfo->partialColFrom + stbInfo->partialColNum; ++i) {
                 Field * col = benchArrayGet(stbInfo->cols, i);
                 n = snprintf(stbInfo->partialColNameBuf+pos,
                                 TSDB_MAX_ALLOWED_SQL_LEN - pos,
@@ -1636,12 +1636,12 @@ int prepareSampleData(SDataBase* database, SSuperTable* stbInfo) {
             }
             
             // first part set noen
-            for (int i = 0; i < =stbInfo->partialColFrom; ++i) {
+            for (uint32_t i = 0; i <= stbInfo->partialColFrom; ++i) {
                 Field * col = benchArrayGet(stbInfo->cols, i);
                 col->none = true;
             }
             // last part set none
-            for (int i = stbInfo->partialColNum; i < stbInfo->cols->size; ++i) {
+            for (uint32_t i = stbInfo->partialColFrom + stbInfo->partialColNum; i < stbInfo->cols->size; ++i) {
                 Field * col = benchArrayGet(stbInfo->cols, i);
                 col->none = true;
             }
