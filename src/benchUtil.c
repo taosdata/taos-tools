551ad36f94d4b240496828d2d23bed3dc783a672
Alex Duan <417921451@qq.com>
2024-04-01 14:23:09 +0800

    enh: taosBenchmark -M -F default action

--- src/benchUtil.c
+++ src/benchUtil.c
@@ -1253,8 +1253,3 @@ FORCE_INLINE void printErrCmdCodeStr(char *cmd, int32_t code, TAOS_RES *res) {
     taos_free_result(res);
 }
 
-FORCE_INLINE void printWarnCmdCodeStr(char *cmd, int32_t code, TAOS_RES *res) {
-    warnPrint("failed to run command %s, code: 0x%08x, reason: %s\n",
-               cmd, code, taos_errstr(res));
-    taos_free_result(res);
-}
