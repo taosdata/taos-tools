c2bef181bac6dcc88e49d583024f758b0f284b24
Alex Duan <417921451@qq.com>
2024-10-16 19:28:06 +0800

    fix: hostname can not set default value

--- src/benchMain.c
+++ src/benchMain.c
@@ -56,6 +56,23 @@ void checkArgumentValid() {
                    g_arguments->prepared_rand, g_arguments->reqPerReq);
         g_arguments->reqPerReq = g_arguments->prepared_rand;
     }
+
+    if(g_arguments->host == NULL) {
+        g_arguments->host = DEFAULT_HOST;
+    }
+
+    if (isRest(g_arguments->iface)) {
+        if (0 != convertServAddr(g_arguments->iface,
+                                 false,
+                                 1)) {
+            errorPrint("%s", "Failed to convert server address\n");
+            return;
+        }
+        encodeAuthBase64();
+        g_arguments->rest_server_ver_major =
+            getServerVersionRest(g_arguments->port);
+    }
+
 }
 
 int main(int argc, char* argv[]) {
