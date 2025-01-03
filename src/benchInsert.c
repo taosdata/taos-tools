9aefb6621a07c98a67ae2cf99ae0e87a82dee116
Alex Duan <417921451@qq.com>
2025-01-03 11:07:50 +0800

    enh: remove stmt2 retry function

--- src/benchInsert.c
+++ src/benchInsert.c
@@ -1805,6 +1805,13 @@ int32_t submitStmt2Impl(threadInfo * pThreadInfo, TAOS_STMT2_BINDV *bindv, int64
     return code;
 }
 
+// no retry function submitStmt2
+int32_t submitStmt2(threadInfo * pThreadInfo, TAOS_STMT2_BINDV *bindv, int64_t *delay1, int64_t *delay3,
+                    int64_t* startTs, int64_t* endTs, uint32_t* generated, int32_t w) {
+    return submitStmt2Impl(pThreadInfo, bindv, delay1, delay3, startTs, endTs, generated);
+}
+
+/*
 int32_t submitStmt2(threadInfo * pThreadInfo, TAOS_STMT2_BINDV *bindv, int64_t *delay1, int64_t *delay3,
                     int64_t* startTs, int64_t* endTs, uint32_t* generated, int32_t w) {
     // calc loop
@@ -1860,6 +1867,7 @@ int32_t submitStmt2(threadInfo * pThreadInfo, TAOS_STMT2_BINDV *bindv, int64_t *
     // success
     return 0;
 }
+*/
 
 static void *syncWriteInterlace(void *sarg) {
     threadInfo * pThreadInfo = (threadInfo *)sarg;
