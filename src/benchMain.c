96548982dc812c04bef119043cb0e4421b9b4759
Alex Duan <417921451@qq.com>
2025-04-24 13:20:44 +0800

    enh: add free1

--- src/benchMain.c
+++ src/benchMain.c
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
     int ret = 0;
 
 
-    char name = malloc(10);
+    char* name = malloc(10);
     strcpy(name, "hello, welcome to use taosBenchmark.");
     free(name);
 
